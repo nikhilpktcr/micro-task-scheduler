196e3b5fa5e595e0175a66e00ddef943dc76580b
nikhilpktcr <nikhilpktcr@gmail.com>
2024-07-03 19:44:32 +0200

    fix

---
+++ index.js
@@ -0,0 +1,48 @@
+import { CronJob } from "cron";
+
+const tasks = new Map();
+
+export const addTask = (name, cronTime, taskFunction) => {
+  if (!tasks.has(name)) {
+    const job = new CronJob(cronTime, taskFunction, null, false, null);
+    tasks.set(name, { job });
+  } else {
+    throw new Error("Task already exists!");
+  }
+};
+
+export const removeTask = (name) => {
+  const task = tasks.get(name);
+  if (!task) {
+    throw new Error("no task found");
+  } else {
+    task.job.stop();
+    tasks.delete(name);
+  }
+};
+
+export const startTask = () => {
+  const task = tasks.get(name);
+  if (!task) {
+    throw new Error("no task found");
+  } else {
+    task.job.start();
+  }
+};
+
+export const stopTask = (name) => {
+  const task = tasks.get(name);
+  if (!task) {
+    throw new Error("no task found");
+  } else {
+    task.job.stop();
+  }
+};
+
+export const startAll = () => {
+  tasks.forEach((task) => task.job.start());
+};
+
+export const stopAll = () => {
+  tasks.forEach((task) => task.job.stop());
+};
